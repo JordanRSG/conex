297443f919950b23728f8abae02eab61bd4f7480
JordanRSG <jordanricardo9@hotmail.com>
2018-04-17 09:30:16 -0300

    first commit

---
+++ CadastroPF/src/controller/PessoaFisicaJdbcDAO.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.PessoaFisica;
+
+public class PessoaFisicaJdbcDAO {
+	private Connection conn;
+	public PessoaFisicaJdbcDAO(Connection conn) {
+		this.conn = conn;
+	}
+	public void salvar(PessoaFisica c) throws SQLException{
+		String sql = "insert into pessoafisica (nome, endereco, bairro, cep, cidade, estado, telefone, celular, rg, cpf, sexo) values ('"+c.getNome()+"','"+c.getEndereco()+"','"+c.getBairro()+"','"+c.getCep()+"','"+c.getCidade()+"','"+c.getEstado()+"','"+c.getTelefone()+"','"+c.getCelular()+"','"+c.getNome()+"','"+c.getCelular()+"','"+c.getRg()+"','"+c.getCpf()+"','"+c.getSexo()+"')";
+		System.out.println(sql);
+		PreparedStatement prepareStatement = this.conn.prepareStatement(sql);
+		prepareStatement.executeUpdate();
+		prepareStatement.close();
+	}
+	public void imprimir() throws SQLException{
+		java.sql.Statement s = conn.createStatement();		
+		ResultSet sql = s.executeQuery("select * from pessoafisica");
+		while(sql.next()) {
+			String all = sql.getString("*");
+			System.out.println(all);
+		}
+	}
+}
